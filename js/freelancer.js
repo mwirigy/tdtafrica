2545c05fd91f97765f93a699a53bf9dccc47c950
mwirigy <mwirigi.charles@gmail.com>
2019-01-29 22:40:48 +0300

    Initial Commit

---
+++ js/freelancer.js
@@ -0,0 +1,116 @@
+
+
+
+
+
+(function($) {
+    "use strict"; // Start of use strict
+    
+    //disable wow animation for mobile content
+    
+    
+    
+    $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+    
+    //navbar collapse when item is clicked
+    $('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Floating label headings for the contact form
+    $(function() {
+        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function() {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function() {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+
+})(jQuery); // End of use strict
+
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+
+//Scroller
+jQuery(document).ready(function() {
+	jQuery("body").niceScroll({
+		cursorcolor:"#333",
+		cursorborder:"0px",
+		cursorwidth :"8px",
+		zindex:"9999"
+	});
+    
+    wow = new WOW(
+		{
+			mobile: false
+		});
+		wow.init();
+});
+
+
+
+$(document).ready(function(){
+	$('.navbar-toggle').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+ var $container = $('#portfolio-container');
+    $container.isotope({
+        filter: '*',
+    });
+
+    $('.portfolio-filter a').click(function () {
+        $('.portfolio-filter .active').removeClass('active');
+        $(this).addClass('active');
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 500,
+                animationEngine: "jquery"
+            }
+        });
+        return false;
+    });
+
